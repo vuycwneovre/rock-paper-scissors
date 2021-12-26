178a23d4685468308238a04b4641c41d307a4cb5
vuycwneovre <jmcasmen@protonmail.com>
2021-12-26 15:57:09 +0100

    ui complete

--- myscripts.js
+++ myscripts.js
@@ -1,27 +1,17 @@
 /* rock beats scissors 1-->3
 scissors beats paper 3-->2
 paper beats rock 2-->1*/
-
-
 const rock = 1;
 const paper = 2;
 const scissors = 3;
 
-
-
-
 /* computerPlay generates 
 random rock, paper, scissors
-
 each thing i.e. rock, paper...
 has one number associated */
-
-
-// Computer
 function computerPlay(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
 const computerSelection = computerPlay(1,3);
 
 
@@ -29,22 +19,17 @@ const computerSelection = computerPlay(1,3);
 player inputs rock, paper or scissors
 in the console, save input in playerSelection
 to compare and see who wins */
-
-
-// Player
-console.log("Write your choice (paper, rock, scissors): ")
-
 function playerPlay(input) {
-    return input
+    console.log(input);
+    return input;
 }
-
 const playerSelection = playerPlay();
 
 
 /* plays one round of rock, paper, scissors
 compares numbers, decides who wins */
-
 function who_wins(computerSelection, playerSelection) {
+    
     // Tie
     if (computerSelection === playerSelection){
         return "Tie!";
@@ -52,46 +37,30 @@ function who_wins(computerSelection, playerSelection) {
         
     //Computer wins
     if (computerSelection === 1 && playerSelection === 3){
-        score_computer++;
+        //score_computer++;
         return "Rock beats scissors, computer wins"
     }
     if (computerSelection === 3 && playerSelection === 2){
-        score_computer++;
+        //score_computer++;
         return "Scissors beats paper, computer wins"
     }
     if (computerSelection === 2 && playerSelection === 1){
-        score_computer++;
+        //score_computer++;
         return "Paper beats rock, computer wins"
     }
         
         
     //Player wins
     if(computerSelection === 3 && playerSelection === 1){
-        score_player++;
+        //score_player++;
         return "Rock beats scissors, player wins"
     }
     if (computerSelection === 2 && playerSelection === 3){
-        score_player++;
+        //score_player++;
         return "Scissors beats paper, player wins"
     }
     if (computerSelection === 1 && playerSelection === 2){
-        score_player++;
+        //score_player++;
         return "Paper beats rock, player wins"
     }
-
-
-    if (score_player > score_computer) {
-        return "Player wins"
-    } 
-    if (score_player < score_computer) {
-        return "Computer wins"
-    }
-    if (score_player === score_computer) {
-        return "Tie"
-    }
-}
-
-
-
-
-
+} 
